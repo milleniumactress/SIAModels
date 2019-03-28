2ba0ebf1bdb9ef5af45569e0deb4f3e36db5d158
Gerry <gerry_faith@hotmail.com>
2019-03-28 13:10:09 +0700

    update matakuliah
    aif133

---
+++ test/id/ac/unpar/siamodels/matakuliah/kurikulum2018/AIF134455Test.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.ac.unpar.siamodels.matakuliah.kurikulum2018;
+
+import id.ac.unpar.siamodels.MataKuliah;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Gerry
+ */
+public class AIF134455Test {
+    
+            private AIF134455 matkul;
+    
+    public AIF134455Test() {
+        this.matkul = new AIF134455();
+    }
+    
+    @Test
+    public void testConstructor() {
+        MataKuliah m = new AIF134455();
+        assertEquals(this.matkul,m);
+    }
+    
+    
+    
+}
