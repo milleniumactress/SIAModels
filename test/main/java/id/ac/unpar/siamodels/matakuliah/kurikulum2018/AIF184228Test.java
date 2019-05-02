942d3dee521ab4461a5da79927464dcf42c2015c
Franky Ang <frankyang53@gmail.com>
2019-05-02 13:47:15 +0700

    Add AIF184* Test

---
+++ test/main/java/id/ac/unpar/siamodels/matakuliah/kurikulum2018/AIF184228Test.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package id.ac.unpar.siamodels.matakuliah.kurikulum2018;
+
+import id.ac.unpar.siamodels.Mahasiswa;
+import id.ac.unpar.siamodels.TahunSemester;
+import java.util.LinkedList;
+import java.util.List;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author asus a455l
+ */
+public class AIF184228Test {
+    
+    /**
+     * Tes mahasiswa dapat mengambil matakuliah AIF184228
+     */
+    @Test
+    public void testCheckPrasyaratTrue() {
+        Mahasiswa mahasiswa = new Mahasiswa("2018730041");
+        List<String> reasonsContainer = new LinkedList<>();
+        AIF184228 instance = new AIF184228();
+        mahasiswa.getRiwayatNilai().add(new Mahasiswa.Nilai(new TahunSemester("182"), new AIF182210(), "A"));
+        mahasiswa.getRiwayatNilai().add(new Mahasiswa.Nilai(new TahunSemester("182"), new AIF183204(), "A"));
+        boolean expResult = true;
+        boolean result = instance.checkPrasyarat(mahasiswa, reasonsContainer);
+        assertEquals(expResult, result);
+    }
+    
+    /**
+     * Tes mahasiswa tidak dapat mengambil matakuliah AIF184228.
+     */
+    @Test
+    public void testCheckPrasyaratFalse() {
+        Mahasiswa mahasiswa = new Mahasiswa("2018730041");
+        List<String> reasonsContainer = new LinkedList<>();
+        AIF184228 instance = new AIF184228();
+        boolean expResult = false;
+        boolean result = instance.checkPrasyarat(mahasiswa, reasonsContainer);
+        assertEquals(expResult, result);
+    }
+    
+}
